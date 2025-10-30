425ea3de31a1597c18b6c45fbcb905da900a54cf
Nirojiha <nironirojiha@gmail.com>
2025-10-30 20:56:04 +0530

    implement routes

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,7 +1,16 @@
+// import { Routes } from '@angular/router';
+// import { TestComponent } from './features/Admin/test/test.component';
+
+// export const routes: Routes = [
+//       { path: '', redirectTo: 'test', pathMatch: 'full' },
+//       { path: 'test', component: TestComponent }
+// ];
+
+
 import { Routes } from '@angular/router';
-import { TestComponent } from './features/Admin/test/test.component';
+import { ServiceListComponent } from './features/Admin/pages/service-list/service-list.component'
 
 export const routes: Routes = [
-      { path: '', redirectTo: 'test', pathMatch: 'full' },
-      { path: 'test', component: TestComponent }
+  { path: '', redirectTo: 'services', pathMatch: 'full' },
+  { path: 'services', component: ServiceListComponent }
 ];
