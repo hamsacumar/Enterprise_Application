765302a0706010b4d0d7c09065a891391fc75533
Nirojiha <nironirojiha@gmail.com>
2025-10-30 21:15:32 +0530

    Fix ServiceItem POST creation

--- Backend/AdminService/obj/Debug/net9.0/Admin_Service.AssemblyInfo.cs
+++ Backend/AdminService/obj/Debug/net9.0/Admin_Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Admin_Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+814a5bacf5abf1de0a1ccb74e7424757c9dd03c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6141597a9fc3678b8b9675b502ec7cfefa9085d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Admin_Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Admin_Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
