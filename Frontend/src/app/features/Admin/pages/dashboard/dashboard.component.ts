92aa59dd82909735f3acfc4abc38774f03ea18fb
Nirojiha <nironirojiha@gmail.com>
2025-10-31 13:36:38 +0530

    feat(admin): create DashboardComponent and SidebarComponent

---
+++ Frontend/src/app/features/Admin/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from '../../pages/sidebar/sidebar.component';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
+  standalone: true,
+  imports: [SidebarComponent, RouterOutlet]
+})
+export class DashboardComponent {}
