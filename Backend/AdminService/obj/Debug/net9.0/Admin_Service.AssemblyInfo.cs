8266f6e21fa14068540997aad76e7b11f81f4249
Nirojiha <nironirojiha@gmail.com>
2025-10-31 23:18:58 +0530

    changes

--- Backend/AdminService/obj/Debug/net9.0/Admin_Service.AssemblyInfo.cs
+++ Backend/AdminService/obj/Debug/net9.0/Admin_Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Admin_Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e4f49c42307124d501fd8e549443b784ff4845a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1e753db107fb9332192984fd9a5de471df64695")]
 [assembly: System.Reflection.AssemblyProductAttribute("Admin_Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Admin_Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
