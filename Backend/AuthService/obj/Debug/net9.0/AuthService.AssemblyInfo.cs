8bb2657f672197407bd2acd852ab8b4138ed902f
Hasini1919 <hasini200219@gmail.com>
2025-11-01 14:10:35 +0530

    Remove ignored files from repository

--- Backend/AuthService/obj/Debug/net9.0/AuthService.AssemblyInfo.cs
+++ Backend/AuthService/obj/Debug/net9.0/AuthService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a75941d72826dc55ed5872eb9e9607e5ed71a10e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0d17edc2b8d2c054bac873c82ea00a3d425da2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
