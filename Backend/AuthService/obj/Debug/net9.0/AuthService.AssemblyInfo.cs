654617d94c50a62145c750ff382407570d1d1513
Hasini1919 <hasini200219@gmail.com>
2025-11-01 14:04:36 +0530

    merge Hasini

--- Backend/AuthService/obj/Debug/net9.0/AuthService.AssemblyInfo.cs
+++ Backend/AuthService/obj/Debug/net9.0/AuthService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a75941d72826dc55ed5872eb9e9607e5ed71a10e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e99178422b8a0b0e387e7e65ba65f844241802f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
