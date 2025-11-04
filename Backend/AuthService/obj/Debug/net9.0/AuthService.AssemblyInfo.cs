5573c1e0dbc30c0f3ae828cffb4189d4804b733a
Tishanth-07 <139070316+Tishanth-07@users.noreply.github.com>
2025-11-05 02:53:15 +0530

    Merge from main

--- Backend/AuthService/obj/Debug/net9.0/AuthService.AssemblyInfo.cs
+++ Backend/AuthService/obj/Debug/net9.0/AuthService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e99178422b8a0b0e387e7e65ba65f844241802f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00390d583fa893ea36ef0144fca1eaffd8d3f1fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
