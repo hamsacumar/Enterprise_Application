47bb6ca23b20f778e3aacefd6f78ddefa879bf6c
Nirojiha <nironirojiha@gmail.com>
2025-11-07 17:17:20 +0530

    routes working correctly

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http'; 
+import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
