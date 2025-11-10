5bc493efe4bf5b74fa38e4f58fa245a8c817da7b
Nirojiha <nironirojiha@gmail.com>
2025-11-10 22:17:35 +0530

    feat(admin-service): add Customer model to store classified customer data

---
+++ Backend/AdminService/Models/Customer.cs
@@ -0,0 +1,8 @@
+public class Customer
+{
+    public string Username { get; set; }
+    public string Address { get; set; }
+    public string CarModel { get; set; }
+    public string CarLicensePlate { get; set; }
+    public string PhoneNumber { get; set; }
+}
