a0d83ad3748731a91a29582a330e4cc9b3b664e4
Rishi <rishigobanjr2002@gmail.com>
2025-11-11 15:31:15 +0530

    full code

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -44,7 +44,6 @@ import { WorkerDashboardComponent } from './features/Worker/pages/worker-dashboa
 // 🚦 ROUTE CONFIGURATION
 // ==========================
 import { Routes } from '@angular/router';
-import { TestComponent } from './features/chat/test/test.component';
 import { PaymentFormComponent } from './features/PaymentGateway/Components/payment-form/payment-form.component';
 import { PaymentStatusComponent } from './features/PaymentGateway/Components/payment-status/payment-status.component';
 
@@ -133,6 +132,9 @@ export const routes: Routes = [
       },
     ],
   },
+  { path: 'payment/success', component: PaymentStatusComponent }, 
+  { path: 'payment/cancel', component: PaymentStatusComponent },
+  {path: 'payment', component: PaymentFormComponent},
 
    // 👇 Wildcard must come LAST
   { path: '**', redirectTo: '/login' },
@@ -147,8 +149,5 @@ export const routes: Routes = [
 //   exports: [RouterModule],
 // })
 // export class AppRoutingModule {}
-      { path: 'payment/success', component: PaymentStatusComponent }, // Handles RETURN_URL
-      { path: 'payment/cancel', component: PaymentStatusComponent },
-      { path: '', redirectTo: 'payment', pathMatch: 'full' },
-      {path: 'payment', component: PaymentFormComponent}
-];
+      
+
